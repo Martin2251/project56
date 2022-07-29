3c25247a8fe43dc62c2d8883e2053ed215674409
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-07-29 14:55:37 +0100

    layout wrapper

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FC } from 'react'
+import {LayoutWrapper} from "./layout.style"
+
+const index = () => {
+  return (
+    <div>
+    <LayoutWrapper>
+
+
+    </LayoutWrapper>
+
+
+    </div>
+  )
+}
+
+export default index
